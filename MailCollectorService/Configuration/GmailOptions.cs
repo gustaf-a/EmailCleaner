5eda58ef7c4a0e7acf402033b842ee6073c382fa
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-21 13:51:58 +0200

    Update configurations and appsettings.json

---
+++ MailCollectorService/Configuration/GmailOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace MailCollectorService.Configuration
+{
+    public class GmailOptions
+    {
+        public const string Gmail = "Gmail";
+
+        public string ApplicationName { get; set; } = string.Empty;
+        public string CredentialsFileName { get; set; } = string.Empty;
+        public string UserId { get; set; } = "me";
+    }
+}
