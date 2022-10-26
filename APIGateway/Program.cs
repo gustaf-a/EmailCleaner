76480b1c90dc1f60a179b5b90901570227613595
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-26 11:15:27 +0200

    [APIGateway] add startup file to prepare for service tests

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,40 +1,23 @@
 using Serilog;
 
-namespace APIGateway
+namespace APIGateway;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.File("logs/apigateway.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.File("apigateway.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
 
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        var startup = new Startup(builder.Configuration);
+        startup.ConfigureServices(builder.Services);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
+        startup.Configure(app, app.Environment);
     }
 }
---
+++ APIGateway/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace APIGateway
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddHttpClient();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
