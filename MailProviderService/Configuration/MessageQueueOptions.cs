9536f34d5d5d1ae59eaeb22a2a7f4122159e5bb5
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-27 13:09:34 +0200

    [MailProviderService] add MessageQueueOptions

---
+++ MailProviderService/Configuration/MessageQueueOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MailProviderService.Configuration;
+
+public class MessageQueueOptions
+{
+    public const string MessageQueue = "MessageQueue";
+
+    public string Exchange { get; set; } = string.Empty;
+    public string HostName { get; set; } = string.Empty;
+    public string RoutingKeyCollected { get; set; } = string.Empty;
+}
