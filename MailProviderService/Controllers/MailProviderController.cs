24a9840dbdea9a09a5a9dea00d4c392047945b57
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-27 13:12:16 +0200

    [MailProviderService] add messageQueue and emailStore to controller

--- MailProviderService/Controllers/MailProviderController.cs
+++ MailProviderService/Controllers/MailProviderController.cs
@@ -1,4 +1,6 @@
 ﻿using MailProviderService.Data;
+using MailProviderService.EmailStore;
+using MailProviderService.MessageQueue;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MailProviderService.Controllers
@@ -7,26 +9,36 @@ namespace MailProviderService.Controllers
     [Route("v1/mailprovider")]
     public class MailProviderController
     {
-        public MailProviderController()
-        {
+        private readonly IMessageQueue _messageQueue;
 
+        private readonly IEmailStore _emailStore;
+
+        public MailProviderController(IMessageQueue messageQueue, IEmailStore emailStore)
+        {
+            _emailStore = emailStore;
+            _messageQueue = messageQueue;
         }
 
-        [HttpGet("ping")]
+        [HttpGet]
         public string Ping()
             => "pong";
 
-        [HttpGet]
+        [HttpGet("collect/start")]
+        public void StartCollecting()
+        {
+            _messageQueue.StartCollecting();
+        }
+
+        [HttpGet("collect/stop")]
+        public void StopCollecting()
+        {
+            _messageQueue.StopCollecting();
+        }
+
+        [HttpGet("emails")]
         public async Task<List<Email>> GetEmails()
         {
-            throw new NotImplementedException();
+            return await _emailStore.GetEmails();
         }
-
-        [HttpPost]
-        public async Task AddEmails()
-        {
-
-        }
-        //TODO Add emails to store
     }
 }
