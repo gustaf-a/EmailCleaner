e84549ebcf2fbe1408f9c6016433b0d7ff367097
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-27 13:07:26 +0200

    [MailProviderService] add startup file

--- MailProviderService/Program.cs
+++ MailProviderService/Program.cs
@@ -7,31 +7,16 @@ public class Program
     {
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
-            .WriteTo.File("logs/mailprovider.txt", rollingInterval: RollingInterval.Day)
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
 
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        var startup = new Startup(builder.Configuration);
+        startup.ConfigureServices(builder.Services);
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        startup.Configure(app, app.Environment);
     }
 }
---
+++ MailProviderService/Startup.cs
@@ -0,0 +1,48 @@
+﻿using MailProviderService.EmailStore;
+using MailProviderService.MessageConsumer;
+using MailProviderService.MessageQueue;
+
+namespace MailProviderService;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddHttpClient();
+
+        services.AddSingleton<IChannelBuilder, RabbitMqChannelBuilder>();
+        services.AddSingleton<IEmailStore, EmailStoreV0>();
+        services.AddSingleton<IMessageConsumerFactory, MessageConsumerFactory>();
+        services.AddSingleton<IMessageQueue, RabbitMqMessageQueue>();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
