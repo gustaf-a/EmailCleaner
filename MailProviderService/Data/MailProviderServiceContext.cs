e78c4d0366cb5fcc6b07542b71f961d0c4cd555e
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-10-31 18:22:24 +0100

    [MailProviderService] add MailProviderServiceContext.cs

---
+++ MailProviderService/Data/MailProviderServiceContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MailProviderService.Data;
+
+public class MailProviderServiceContext : DbContext
+{
+	public MailProviderServiceContext(DbContextOptions<MailProviderServiceContext> options)
+		: base(options) {}
+
+	public DbSet<Email> Email { get; set; }
+}
