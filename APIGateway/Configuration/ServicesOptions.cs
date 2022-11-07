97be6d00991e3dd44bf77244030f8dee15d12fac
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-11-07 16:01:43 +0100

    [APIGateway] add ServicesOptions

---
+++ APIGateway/Configuration/ServicesOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace APIGateway.Configuration;
+
+public class ServicesOptions
+{
+    public const string Services = "Services";
+
+    public string MailCollectorServiceUri { get; set; } = "http://localhost:5201/";
+    public string MailProviderServiceUri { get; set; } = "http://localhost:5202/";
+}
