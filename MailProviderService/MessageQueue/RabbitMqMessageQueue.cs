3500a48d3d964782a28e606148249ba9f96a4b2e
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-11-17 09:51:44 +0100

    [MailProviderService] remove unused MessageQueueOptions from RabbitMqMessageQueue

--- MailProviderService/MessageQueue/RabbitMqMessageQueue.cs
+++ MailProviderService/MessageQueue/RabbitMqMessageQueue.cs
@@ -1,22 +1,16 @@
 ﻿using RabbitMQ.Client;
-using MailProviderService.Configuration;
 using MailProviderService.MessageConsumer;
-using Serilog;
 
 namespace MailProviderService.MessageQueue
 {
     public class RabbitMqMessageQueue : IMessageQueue
     {
-        private readonly MessageQueueOptions _messageQueueOptions;
-
         private readonly IBasicConsumer _consumer;
 
         private readonly IChannelHandler _channelHandler;
 
-        public RabbitMqMessageQueue(IConfiguration configuration, IChannelHandler channelHandler, IMessageConsumerFactory messageConsumerFactory)
+        public RabbitMqMessageQueue(IChannelHandler channelHandler, IMessageConsumerFactory messageConsumerFactory)
         {
-            _messageQueueOptions = configuration.GetSection(MessageQueueOptions.MessageQueue).Get<MessageQueueOptions>();
-
             _channelHandler = channelHandler;
 
             _consumer = messageConsumerFactory.Build(_channelHandler.GetChannel()) ;
