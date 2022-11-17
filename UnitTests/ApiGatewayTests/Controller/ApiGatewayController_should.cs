4b152dd498488ee5ced95d73f6edcbb74c20bde0
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-11-17 09:54:40 +0100

    [APIGateway] add service tests

--- UnitTests/ApiGatewayTests/Controller/ApiGatewayController_should.cs
+++ UnitTests/ApiGatewayTests/Controller/ApiGatewayController_should.cs
@@ -1,23 +1,37 @@
 ﻿using APIGateway;
+using APIGateway.Data;
+using ApiGatewayTests.Mocks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace ApiGatewayTests.Controller;
 
 public class ApiGatewayController_should : 
     IClassFixture<WebApplicationFactory<Startup>>
 {
-    private readonly HttpClient _client;
+    private readonly HttpClient _apiGatewayclient;
+
+    private Mock<HttpMessageHandler> _httpMessageHandlerMock;
 
     private const string ControllerRoute = "v1/emailcleaner";
 
     public ApiGatewayController_should(WebApplicationFactory<Startup> factory)
     {
-        _client = factory
+        _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+        var httpClientUsedByApiGateway = new HttpClient(_httpMessageHandlerMock.Object);
+        var httpClientFactoryMock = new FakeHttpClientFactory(httpClientUsedByApiGateway);
+
+        _apiGatewayclient = factory
             .WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
                 {
-                    
+                    services.AddSingleton<IHttpClientFactory>((services) => httpClientFactoryMock);
                 });
             })
             .CreateClient();
@@ -26,34 +40,78 @@ public class ApiGatewayController_should :
     [Fact]
     public async Task GetPingResponse()
     {
-        var pingResponse = await _client.GetAsync($"{ControllerRoute}");
-        pingResponse.EnsureSuccessStatusCode();
+        var response = await _apiGatewayclient.GetAsync($"{ControllerRoute}");
+        response.EnsureSuccessStatusCode();
     }
 
     [Fact]
     public async Task Send_request_to_mailCollectorService_to_start()
     {
-        var pingResponse = await _client.GetAsync($"{ControllerRoute}/collect/start");
-        pingResponse.EnsureSuccessStatusCode();
+        //Arrange
+        _httpMessageHandlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Returns(Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK
+            }));
 
-        //TODO Ensure sends to service
+        //Act
+        var response = await _apiGatewayclient.GetAsync($"{ControllerRoute}/collect/start");
+        response.EnsureSuccessStatusCode();
+
+        //Assert
+        Assert.Equal(2, _httpMessageHandlerMock.Invocations.Count);
     }
 
     [Fact]
     public async Task Send_request_to_mailCollectorService_to_stop()
     {
-        var pingResponse = await _client.GetAsync($"{ControllerRoute}/collect/stop");
-        pingResponse.EnsureSuccessStatusCode();
+        //Arrange
+        _httpMessageHandlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Returns(Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK
+            }));
 
-        //TODO Ensure sends to service
+        //Act
+        var response = await _apiGatewayclient.GetAsync($"{ControllerRoute}/collect/stop");
+        response.EnsureSuccessStatusCode();
+
+        //Assert
+        Assert.Equal(2, _httpMessageHandlerMock.Invocations.Count);
     }
 
     [Fact]
     public async Task Get_emails_from_provider_service()
     {
-        var pingResponse = await _client.GetAsync($"{ControllerRoute}/collect");
-        pingResponse.EnsureSuccessStatusCode();
+        //Arrange
+        var content = new StringContent(JsonConvert.SerializeObject(new List<Email>
+        {
+            new Email
+            {
+                Id = "5"
+            }
+        }));
 
-        //TODO Ensure sends to service
+        _httpMessageHandlerMock.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Returns(Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = content
+            }));
+
+        //Act
+        var response = await _apiGatewayclient.GetAsync($"{ControllerRoute}/collect");
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        
+        Assert.Equal(1, _httpMessageHandlerMock.Invocations.Count);
+
+        var emails = await response.Content.ReadFromJsonAsync<List<Email>>();
+        Assert.Single(emails);
+        Assert.Equal("5", emails[0].Id);
     }
 }
---
+++ UnitTests/ApiGatewayTests/Mocks/FakeHttpClientFactory.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ApiGatewayTests.Mocks
+{
+    internal class FakeHttpClientFactory : IHttpClientFactory
+    {
+        private HttpClient _httpClient;
+
+        public FakeHttpClientFactory(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public HttpClient CreateClient(string name)
+        {
+            return _httpClient;
+        }
+    }
+}
