5b54f90e9641b3615db7c4f30620aa7e727bdfe2
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-11-17 09:47:40 +0100

    [APIGateway] add properties to Email class

--- APIGateway/Data/Email.cs
+++ APIGateway/Data/Email.cs
@@ -2,5 +2,11 @@
 {
     public class Email
     {
+        public string Origin { get; set; }
+        public string Id { get; set; }
+        public string ThreadId { get; set; }
+        public string LabelIds { get; set; }
+        public string Snippet { get; set; }
+        public string Payload { get; set; }
     }
 }
